c580fe50ba9e56ceeedbe6c2ebce9bb5732ff088
Guillermo Royo <guillermo.royo.rivera@gmail.com>
2023-12-03 01:06:07 -0300

    fix: material

---
+++ app02_gestor_personas/main.js
@@ -0,0 +1,35 @@
+
+const {app, BrowserWindow} = require('electron');
+const path = require('path');
+const electronReload = require('electron-reload');
+
+electronReload(__dirname);
+
+function crearVentanaPrincipal() {
+    let ventanaPrincipal = new BrowserWindow({
+        width:800,
+        height:600,
+        webPreferences: {
+            nodeIntegration: true,
+            enableRemoteModule: true,
+            contextIsolation: false,
+        }
+    });
+
+    ventanaPrincipal.loadFile('index.html');
+
+} 
+
+app.whenReady().then(crearVentanaPrincipal)
+
+app.on('window-all-closed', function (){
+    if (process.platform !== 'darwin'){
+        app.quit();
+    }
+});
+
+app.on('activate', function (){
+    if(BrowserWindow.getAllWindows().length === 0){
+        crearVentanaPrincipal();
+    }
+});
