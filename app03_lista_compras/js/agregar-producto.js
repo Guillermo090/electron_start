131b18acbc8980431d3bbf1c8560b8caeaa16f3d
Guillermo Royo <guillermo.royo.rivera@gmail.com>
2023-12-20 21:47:46 -0300

    agregarproducto js function

--- app03_lista_compras/js/agregar-producto.js
+++ app03_lista_compras/js/agregar-producto.js
@@ -2,7 +2,11 @@ const {ipcRenderer} = require('electron');
 
 function agregarProducto (evento) {
     evento.preventDefault();
-    
+    let nombreProducto = document.querySelector.apply('nombreProducto').value;
+
+    if(nombreProducto){
+        ipcRenderer.send('producto:agregar',)
+    }
 }
 
 document.querySelector('#frmAgregarProducto').addEventListener('submit', agregarProducto)
